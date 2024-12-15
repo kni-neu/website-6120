303d808e5e95096a8bd195d672d104ac5061733c
Karl Ni <k.ni@northeastern.edu>
2024-12-15 15:45:06 +0000

    Updating the README.md

--- start-docker.sh
+++ start-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -p 8080:8080 -v $PWD:/home -it --platform linux/amd64 amirpourmand/al-folio /bin/bash
+# Testing
+docker run -p 8060:8080 -v $PWD:/home --rm -it --name neu-cs6120 --platform linux/amd64 amirpourmand/al-folio /bin/bash
+# docker run -p 8080:8080 -v $PWD:/home -it --name neu-cs6120 --platform linux/amd64 amirpourmand/al-folio /bin/bash
+
