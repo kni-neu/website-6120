5700583ed6637dca264c5a355e0fc469ea3d91ce
Karl Ni <k.ni@northeastern.edu>
2025-09-07 05:22:53 +0000

    Deploy Spring 2025 website

---
+++ README.md
@@ -0,0 +1,48 @@
+# CS6120 Natural Language Processing Website Repository
+
+This is the repository for the Natural Language Processing course, taught Spring 2025. 
+
+## Repository structure
+
+This repository is created from a Jekyll template, originally forked from [alshedivat/al-folio](https://github.com/alshedivat/al-folio). 
+
+* **`_data/`**. This folder has syllabus information and lab information. They're stored in `*.yml` files.
+* **`_projects/`**. Somewhat of a misnomer. It's staff information. This folder houses profiles about each person, including their office hours, a picture, and contact information. At the beginning of each semester, we will need to update this with relevant teaching staff.
+  * `1_project.md` - will always be mine
+  * `2_project.md` - will be the head TA
+  * `3_project.md` and `4_project.md` - will be TAs
+
+
+## Building this website locally
+
+This repository builds into [`_website`](./_website), translating Markdown into HTML. It will automatically build (if you set up the Git hooks) when you push to Git. To view your website locally, then follow these steps.
+
+1. Fork [kni-neu/website-6120](`https://github.com/kni-neu/website-6120`) and clone into local folder
+2. Run `./start-docker.sh`
+  * This starts the docker environment, and calls it `neu-cs6120`.
+  * To detach without killing your container, type `Ctrl+P` and then `Ctrl-Q`
+  * To access this container, `docker exec -it neu-cs6120 /bin/bash`
+3. Inside the container, go to the repo folder (e.g., `cd /home/`
+4. Install the packages with `bundle install`
+5. Make any changes to markdown files (see the repository structure above).
+6. Start the server `./start-server.sh`.
+  * Any additional changes should automatically update the server.
+7. On a browser, go to http://localhost:8080.
+  * The port 8080 is whatever you chose to expose
+
+
+## Creating a website externally on CCS
+
+At the beginning of each semester, we will need to create a public-facing website. 
+
+1. E-mail khoury-systems@northeastern.edu for a website
+  - They'll create `/net/course/cs6120s25 (or whatever URL is agreed upon)
+  - This folder will also have `/net/course/cs6120-spXX/.www`
+2. Clone into local folder and create Docker environment with ./start-docker.sh
+  - Edit `baseurl: /cs6120s25` in `_config.yml`
+  - Run `./start-server.sh`
+  - Commit to repository and let auto-hooks run
+3. Login to: `login.khoury.northeastern.edu`
+  - Goto `/net/course/cs6120-spXX/.www`, which will appear as http://course.ccs.neu.edu/cs6120s25.
+  - Clone https://github.com/kni-neu/website-6120.git
+  - Branch and checkout `gh-pages`
---
+++ assets/python/assignment2.py
@@ -0,0 +1,134 @@
+import pickle
+import numpy as np
+import nltk
+from utils import extract_featuers
+
+nltk.download('twitter_samples')
+nltk.download('stopwords')
+
+#@title Q1.1 Sigmoid Inference
+
+def sigmoid( x ):
+  """
+  This function implements the logistic regression algorithm.
+
+  Args:
+    x: A numpy array of shape (d,) or (N, d) representing
+      the input data.
+
+  Returns:
+    y: A numpy array of shape (l,) or (N, l) representing
+      the output data.
+  """
+  return None
+
+#@title Q1.2 One Layer Inference
+
+def inference_layer(X, W, b):
+  """
+  Implements the forward propagation for the logistic regression model.
+
+  Args:
+    X: The input data, of shape (number of features, number of examples).
+    w: Weights, a numpy array of shape (number of features, 1).
+    b: Bias, a scalar.
+
+  Returns:
+    y: The output of shape l or l x N
+  """
+  return None
+
+#@title Q1.3 Two Layer Inference
+
+def inference_2layers(X, W1, w2, b1, b2)
+  """
+  Implements the forward propagation of a two layer neural network
+  model.
+
+  - Let d be the number of features (dimensionality) of the input.
+  - Let N be the number of data samples in your batch
+  - Let H be the number of hidden units
+  
+  Args:
+  X: The input data, with shape either d or d x N
+  W1: Weights for the first weight matrix: shape = H x d
+  w2: Weights for the second matrix: shape = 1 x H
+  b1: Bias value for first layer
+  b2: Bias value for second layer
+
+  Returns:
+  y: Singular output data (1, 0), with shape either 1 or N.
+  """
+  return None
+
+#@title Q1.4 BCE Loss
+
+def bce_forward(yhat, y):
+  """
+  This function calculates the gradient of the logistic regression cost
+  function with respect to the weights and biases.
+
+  Args:
+    yhat: A numpy array of shape (N,) representing the predicted outputs
+    y: A numpy array of shape (N,) representing the target labels.
+
+  Returns:
+    loss_value
+  """
+  return None
+
+#@title Q3.1 Gradients
+
+def gradients(X, y, W1, W2, b1, b2):
+  '''
+  Calculate the gradients of the cost functions with respect to W1, W2, b1, and b2.
+
+  Args:
+    W1: Weight np.array of shape (h, d), first layer
+    b1: Bias np.array of shape (h,), first layer
+    W1: Weight np.array of shape (1, h), second layer
+    b1: Bias value, second layer
+    X: Input data of shape (d, N) representing the input data.
+    y: Target / output labels of shape (N,)
+
+  Returns:
+    dL/dW1: Derivative of cost fxn w/r/t W1
+    dL/db1: Derivative of cost fxn w/r/t b1
+    dL/dW2: Derivative of cost fxn w/r/t W2
+    dL/db2: Derivative of cost fxn w/r/t b2
+    L: Loss value
+  '''
+  return None, None, None, None, None
+
+#@title Q4 Parameter Updates
+
+def update_params(batchx, batchy, W1, B1, W2, b2, lr = 0.01):
+    '''
+    Updates your parameters
+        batchx: Mini-batch of features
+        batchy: Corresponding mini-batch of labels
+        W1: Starting value of W1
+        b1: Starting value of b1
+        W2: Starting value of W2
+        b2: Starting value of b2
+        lr: Learning parameter, default to 0.01
+
+    Returns
+        W1: New value of W1
+        b1: New value of b1
+        W2: New value of W2
+        b2: New value of b2
+    '''
+    return None, None, None, None
+
+#@title Q5 Training the Neural Network
+
+def train_nn(filename, hidden_layer_size, iters=1e6, lr = 0.01):
+    '''
+    Reads in data (twitter_data.pkl), initiates parameters, extracts features
+    and returns the parameters
+
+    Returns:
+        W1, b1, W2, b2
+    '''
+    return None, None, None, None
